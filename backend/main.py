3a2308767539f603e36a75a1eef814cf4449eb64
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-04-03 00:57:23 +0200

    feat: gpt3 prediction

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from flask import request, Flask
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # download required nltk assets
 nltk.download('vader_lexicon')
@@ -9,15 +13,56 @@ sid = SentimentIntensityAnalyzer()
 app = Flask(__name__)
 
 
-@app.route('/', methods=["POST"])
-def example():
+@app.route('/prediction', methods=["POST"])
+def get_prediction():
+    tweet_text = request.json["text"]
+    result = nltk_is_negative(tweet_text)
+
+    # can't detect reliable prediction result
+    # try evaluating with openai gpt3 model
+    if result is None:
+        result = openai_is_negative(tweet_text)
+
     return {
-        'prediction': predict(request.json['text'])
+        'is_negative': result
     }
 
 
-def predict(text):
-    return sid.polarity_scores(text)
+def nltk_is_negative(text):
+    # compound property = aggregated score
+    result = sid.polarity_scores(text)
+    print(result)
+
+    # can't determine in case of neutral prediction
+    if result["neu"] >= result["pos"] and result["neu"] >= result["neg"]:
+        return None
+
+    return result["pos"] <= result["neg"]
+
+
+def openai_is_negative(text):
+    result = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Decide whether a Tweet's sentiment is positive, neutral, or negative.\n\nTweet: \"{text}\"\nSentiment:",
+        temperature=0,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0
+    )
+    print(result)
+    # can't determine if no choices were returned
+    if len(result.choices) == 0:
+        return None
+
+    prediction = result.choices[0].text.strip()
+    if prediction == "Negative":
+        return True
+    elif prediction == "Positive":
+        return False
+    else:
+        # can't determine if predictor equals "Neutral" or some other possible value
+        return None
 
 
 if __name__ == "__main__":
